6cd65b1b9050e438b63297bfafedf2754dbc7a65
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2025-04-27 13:22:21 +0545

    Affiliates improved

--- src/shared/Affiliates.tsx
+++ src/shared/Affiliates.tsx
@@ -15,12 +15,12 @@ const Affiliates = () => {
     <div>
       {isAffiliatesLoading && <Loader/>}
             <p className={`text-primary text-3xl max-sm:text-2xl mt-12 ${antic.className}`}>Our Affiliates</p>
-            <div className='grid grid-cols-4 max-sm:grid-cols-2 gap-4 mt-8'>    
+            <div className='grid grid-cols-6 max-md:grid-cols-5 max-sm:grid-cols-3 gap-4 mt-8'>    
                 {affiliates?.data?.data?.map((item: {affiliatedAccommodation: string, link: string, thumbnail: string}) => (
                         item?.link ? (
                             <Link href={item.link} key={item.affiliatedAccommodation} target='_blank'>
                                 <div className="flex flex-col gap-4 border border-gray-300 rounded-xl hover:border-primary duration-150">
-                                    <div className="w-full h-52 p-2">
+                                    <div className="w-full h-28 p-2">
                                         <Image
                                             src={item?.thumbnail}
                                             alt={item.affiliatedAccommodation}
@@ -29,7 +29,6 @@ const Affiliates = () => {
                                             className="w-full h-full object-cover rounded-md"
                                         />
                                     </div>
-                                    <p className={`${antic.className} text-primary text-center mb-4 font-medium text-2xl max-sm:text-lg`}>{item.affiliatedAccommodation}</p>
                                 </div>
                             </Link>
                         ) : null

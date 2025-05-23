32a8a519a0040319bb78057432b400b4a6030ee9
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2025-05-23 10:10:10 +0900

    AIML and BigData

--- src/components/Destionation/Nepal/TripCard.tsx
+++ src/components/Destionation/Nepal/TripCard.tsx
@@ -1,42 +1,59 @@
-import { antic } from '@/utility/font'
-import { Button } from '@nextui-org/react'
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
-import { Tour } from '@/types/types'
+import { antic } from '@/utility/font';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+import { Tour } from '@/types/types';
 
-const TripCard: React.FC<Tour> = ({ tourName,tourOverview,thumbnail,idealTime,cost,slug  }) => {
-    const slicedDesc = tourOverview?.slice(0, 200)
-    return (
-        <div className="w-full lg:h-[250px] h-auto flex lg:flex-row flex-col shadow-lg relative group overflow-hidden cursor-pointer custom-trip-card">
-            <div className="lg:h-full h-[250px] lg:w-1/3 w-full overflow-hidden relative">
-                <Image
-                    src={thumbnail}
-                    alt="Nepal"
-                    height={1000}
-                    width={1000}
-                    className="object-cover h-full w-full rounded-sm transform transition-transform duration-500 group-hover:scale-110"
-                />
-            </div>
+const TripCard: React.FC<Tour> = ({
+  tourName,
+  tourOverview,
+  thumbnail,
+  idealTime,
+  cost,
+  slug,
+}) => {
+  const slicedDesc = tourOverview?.slice(0, 200);
+  return (
+    <div className="w-full lg:h-[250px] h-auto flex lg:flex-row flex-col shadow-lg relative group overflow-hidden cursor-pointer custom-trip-card">
+      <div className="lg:h-full h-[250px] lg:w-1/3 w-full overflow-hidden relative">
+        <Image
+          src={thumbnail}
+          alt="Nepal"
+          height={1000}
+          width={1000}
+          className="object-cover h-full w-full rounded-sm transform transition-transform duration-500 group-hover:scale-110"
+        />
+      </div>
 
-            <div className="flex flex-col gap-2 lg:px-8 px-4 lg:py-8 py-4 lg:w-2/3 w-full">
-                <h1 className={`${antic.className} text-primary lg:text-3xl text-xl`}>{tourName}</h1>
-                <p className="lg:my-2 my-1 lg:text-sm text-xs">{slicedDesc}</p>
-                <p className={`${antic.className} text-xl text-primary`}>
-                    <span className='text-sm'>Starting from</span> ${cost} <span className="text-sm">/ per person</span>
-                </p>
-                <Link href={`/destinations/nepal/${slug}`}>
-                    <Button className="bg-primary rounded-sm mt-4 text-white px-8" size="sm">
-                        View Details
-                    </Button>
-                </Link>
-            </div>
-
-            <p className="absolute top-0 right-0 py-2 rounded-bl-md px-6 bg-primary text-white text-sm">
-                {idealTime.join(', ')}
-            </p>
+      <div className="flex flex-col gap-2 lg:px-8 px-4 lg:py-8 py-4 lg:w-2/3 w-full">
+        <h1 className={`${antic.className} text-black lg:text-2xl text-xl`}>
+          {tourName}
+        </h1>
+        <p className="lg:my-2 my-1 lg:text-sm text-xs text-black/65">
+          {slicedDesc}
+        </p>
+        <div className="flex items-center justify-between gap-4">
+          <p className={`${antic.className} text-xl text-primary`}>
+            <span className="text-sm">Starting from</span> ${cost}{' '}
+            <span className="text-sm">/ per person</span>
+          </p>
+          <Link href={`/destinations/nepal/${slug}`}>
+            <Button
+              className="bg-primary rounded-sm mt-4 text-white px-8"
+              size="sm"
+            >
+              View Details
+            </Button>
+          </Link>
         </div>
-    )
-}
+      </div>
 
-export default TripCard
+      <p className="absolute top-0 right-0 py-2 rounded-bl-md px-6 bg-primary/5 border-primary border-l border-y text-primary text-xs">
+        {idealTime.join(', ')}
+      </p>
+    </div>
+  );
+};
+
+export default TripCard;
--- src/components/MainHome/BookTrip/BookTrip.tsx
+++ src/components/MainHome/BookTrip/BookTrip.tsx
@@ -1,68 +1,114 @@
-import SharedTitle from '@/shared/SharedTitle'
-import { antic } from '@/utility/font'
-import { Button, Link } from '@nextui-org/react'
-import React from 'react'
-import { FaClock, FaHandsHelping, FaMapMarkerAlt, FaMoneyBillWave, FaPlane } from 'react-icons/fa'
+import SharedTitle from '@/shared/SharedTitle';
+import { antic } from '@/utility/font';
+import { Button, Link } from '@nextui-org/react';
+import React from 'react';
+import {
+  FaClock,
+  FaHandsHelping,
+  FaMapMarkerAlt,
+  FaMoneyBillWave,
+  FaPlane,
+} from 'react-icons/fa';
 
 const BookTrip = () => {
-    return (
-        <div className='w-full flex lg:flex-row flex-col my-16 lg:px-16 px-4 lg:gap-12 gap-4'>
-            <div className='lg:sticky flex flex-col top-4 px-4 py-4 lg:w-1/2 w-full h-fit'>
-                <SharedTitle title='Planning a trip ? ' subtitle='let us help'/>
-                <p className='text-sm text-justify mt-4'>Planning a luxury trip is simple! Start by selecting your dream destination, decide on your ideal trip duration, and estimate your budget. Then, let us step in to craft the perfect itinerary, ensuring every detail is tailored for an exceptional travel experience.</p>
-                <Link href='/tailor-made'><Button className='px-12 mt-6 bg-primary text-white rounded-sm'>Plan trip</Button></Link>
-            </div>
-            <div className='lg:w-1/2 w-full p-4'>
-                <ol className="relative border-l border-primary ">
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
-                    <FaMapMarkerAlt className="text-white" size={22} />
-                    </span>
-                    <h3 className={`text-2xl tracking-wide font-semibold text-primary mb-1 ml-4 ${antic.className}`}>Select Destination</h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
-                    Explore our curated luxury destinations across Nepal, Bhutan, and Tibet, each offering unique experiences, from serene retreats to adventure-filled getaways. Choose the destination that resonates with your travel dreams.
-                    </p>
-                </li>
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
-                    <FaClock className="text-white" size={22} />
-                    </span>
-                    <h3 className={`text-2xl tracking-wide font-semibold text-primary mb-1 ml-4 ${antic.className}`}>Decide Trip Duration</h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
-                    Consider how much time you’d like to immerse yourself in each location. Whether it’s a weekend escape or a multi-week adventure, our team will ensure every moment is memorable.
-                    </p>
-                </li>
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
-                    <FaMoneyBillWave className="text-white" size={22} />
-                    </span>
-                    <h3 className={`text-2xl tracking-wide font-semibold text-primary mb-1 ml-4 ${antic.className}`}>Estimate Budget</h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
-                    Set a budget that aligns with your desired level of comfort and exclusivity. From private tours to five-star accommodations, we’ll tailor the experience to fit your expectations.
-                    </p>
-                </li>
-                <li className="mb-10 ml-6">
-                    <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
-                    <FaHandsHelping className="text-white" size={22} />
-                    </span>
-                    <h3 className={`text-2xl tracking-wide font-semibold text-primary mb-1 ml-4 ${antic.className}`}>Let Us Help</h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
-                    Once you’ve outlined your preferences, our specialists will create a bespoke itinerary designed to meet your unique travel style, offering personalized services and exceptional experiences.
-                    </p>
-                </li>
-                <li className="ml-6">
-                    <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
-                    <FaPlane className="text-white" size={22} />
-                    </span>
-                    <h3 className={`text-2xl tracking-wide font-semibold text-primary mb-1 ml-4 ${antic.className}`}>Plan the Action</h3>
-                    <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
-                    With everything set, all that’s left is for you to prepare for your journey. Pack your essentials and get ready for an extraordinary luxury adventure curated just for you!
-                    </p>
-                </li>
-                </ol>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="w-full flex lg:flex-row flex-col my-16 lg:px-16 px-4 lg:gap-12 gap-4">
+      <div className="lg:sticky flex flex-col top-4 px-4 py-4 lg:w-1/2 w-full h-fit">
+        <SharedTitle title="Planning a trip ? " subtitle="let us help" />
+        <p className="text-sm text-justify mt-4">
+          Planning a luxury trip is simple! Start by selecting your dream
+          destination, decide on your ideal trip duration, and estimate your
+          budget. Then, let us step in to craft the perfect itinerary, ensuring
+          every detail is tailored for an exceptional travel experience.
+        </p>
+        <Link href="/tailor-made">
+          <Button className="px-12 mt-6 bg-primary text-white rounded-sm">
+            Plan trip
+          </Button>
+        </Link>
+      </div>
+      <div className="lg:w-1/2 w-full p-4">
+        <ol className="relative border-l border-primary ">
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
+              <FaMapMarkerAlt className="text-white" size={22} />
+            </span>
+            <h3
+              className={`text-2xl tracking-wide font-semibold text-black mb-1 ml-4 ${antic.className}`}
+            >
+              Select Destination
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
+              Explore our curated luxury destinations across Nepal, Bhutan, and
+              Tibet, each offering unique experiences, from serene retreats to
+              adventure-filled getaways. Choose the destination that resonates
+              with your travel dreams.
+            </p>
+          </li>
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
+              <FaClock className="text-white" size={22} />
+            </span>
+            <h3
+              className={`text-2xl tracking-wide font-semibold text-black mb-1 ml-4 ${antic.className}`}
+            >
+              Decide Trip Duration
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
+              Consider how much time you’d like to immerse yourself in each
+              location. Whether it’s a weekend escape or a multi-week adventure,
+              our team will ensure every moment is memorable.
+            </p>
+          </li>
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
+              <FaMoneyBillWave className="text-white" size={22} />
+            </span>
+            <h3
+              className={`text-2xl tracking-wide font-semibold text-black mb-1 ml-4 ${antic.className}`}
+            >
+              Estimate Budget
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
+              Set a budget that aligns with your desired level of comfort and
+              exclusivity. From private tours to five-star accommodations, we’ll
+              tailor the experience to fit your expectations.
+            </p>
+          </li>
+          <li className="mb-10 ml-6">
+            <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
+              <FaHandsHelping className="text-white" size={22} />
+            </span>
+            <h3
+              className={`text-2xl tracking-wide font-semibold text-black mb-1 ml-4 ${antic.className}`}
+            >
+              Let Us Help
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
+              Once you’ve outlined your preferences, our specialists will create
+              a bespoke itinerary designed to meet your unique travel style,
+              offering personalized services and exceptional experiences.
+            </p>
+          </li>
+          <li className="ml-6">
+            <span className="absolute flex items-center justify-center size-12 bg-primary rounded-full -left-6 ring-8 ring-white ">
+              <FaPlane className="text-white" size={22} />
+            </span>
+            <h3
+              className={`text-2xl tracking-wide font-semibold text-black mb-1 ml-4 ${antic.className}`}
+            >
+              Plan the Action
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400 ml-4">
+              With everything set, all that’s left is for you to prepare for
+              your journey. Pack your essentials and get ready for an
+              extraordinary luxury adventure curated just for you!
+            </p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-export default BookTrip
+export default BookTrip;

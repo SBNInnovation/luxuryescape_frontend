e5b6d3f50a1470ef56e1546c999dbac06bcae1ad
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2025-05-27 16:39:42 +0900

    n

--- src/components/LuxuryTreks/SingleTrek/DestinationandOverview.tsx
+++ src/components/LuxuryTreks/SingleTrek/DestinationandOverview.tsx
@@ -1,27 +1,34 @@
-"use client"
-import React, { useState } from 'react'
-import { LuxuryPackage } from './types'
-import { antic } from '@/utility/font'
-import Image from 'next/image'
-import { Button } from '@nextui-org/react'
-const DestinationandOverview: React.FC<LuxuryPackage> = ({description}) => {
-    const [expanded,setExpaned]=useState(false)
+'use client';
+import React, { useState } from 'react';
+import { LuxuryPackage } from './types';
+import { antic } from '@/utility/font';
+import Image from 'next/image';
+import { Button } from '@nextui-org/react';
+const DestinationandOverview: React.FC<LuxuryPackage> = ({ description }) => {
+  const [expanded, setExpaned] = useState(false);
 
-    const toggleExpanded=()=>{
-        setExpaned(!expanded)
+  const toggleExpanded = () => {
+    setExpaned(!expanded);
+  };
+  return (
+    <>
+      <h1
+        className={`text-3xl ${antic.className} font-semibold text-primary mt-20 mb-8 `}
+      >
+        Trek Overview
+      </h1>
+      <p className="text-justify font-light lg:leading-9 leading-6">
+        {expanded ? description : `${description?.slice(0, 800)}...`}
+      </p>
+      <Button
+        onClick={toggleExpanded}
+        variant="light"
+        className="text-primary underline underline-offset-2 rounded-sm my-4"
+      >
+        {expanded ? 'Show Less' : 'Read More'}
+      </Button>
+    </>
+  );
+};
 
-    }
-    return (
-        <>
-            <h1 className={`text-3xl ${antic.className} font-semibold text-primary mt-20 mb-8 `}>Trek Overview</h1>
-            <p className="text-justify font-light lg:leading-9 leading-6">
-                {expanded ? description : `${description?.slice(0, 800)}...`}
-            </p>
-            <Button onClick={toggleExpanded} variant='light' className="text-primary underline underline-offset-2 rounded-sm my-4">
-                {expanded ? 'Show Less' : 'Read More'}
-            </Button>
-        </>
-    )
-}
-
-export default DestinationandOverview
+export default DestinationandOverview;

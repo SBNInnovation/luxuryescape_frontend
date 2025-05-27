20914d16bb26bd0a24bd8a7fec821c225a6d1a77
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2025-05-27 13:38:26 +0900

    pages

---
+++ src/app/permits/page.tsx
@@ -0,0 +1,12 @@
+import TravelPermit from '@/components/TravelPermit/TravelPermit'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <TravelPermit/>
+        </div>
+    )
+}
+
+export default page
